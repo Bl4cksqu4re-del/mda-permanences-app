8855b20876bf6e3b5c3aceb6daffb9d9cfe1971e
Bl4cksqu4re-del <loic.a.volat@gmail.com>
2026-06-10 14:41:56 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,2 @@
-// Remplacer par l'URL de votre backend Render après déploiement
-// Ex: https://mda-permanences-api.onrender.com
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_URL = 'https://mda-permanences-api.onrender.com';
 export default API_URL;
